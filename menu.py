16740ab9ce57da8cb67efc7f26759cdc2137e6ef
Mevilen Ramsamy <mevilenramsamy@gmail.com>
2025-12-10 15:09:35 -0500

    Ajout des plats principaux

--- menu.py
+++ menu.py
@@ -1,11 +1,12 @@
-def main():
-    print("=== MENU DU RESTAURANT ===")
-
-if __name__ == "__main__":
-    main()
-
 def afficher_plats_principaux():
     print("=== Plats Principaux ===")
     print("- Poulet rôti et légumes grillés")
     print("- Lasagnes maison")
-    print("- Saumon grillé avec riz ")
+    print("- Saumon grillé avec riz basmati")
+
+def main():
+    print("=== MENU DU RESTAURANT ===")
+    afficher_plats_principaux()
+
+if __name__ == "__main__":
+    main()
