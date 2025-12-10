e6126e99e73fc89ede538ccda745bb744f63298e
Samuel Lortie <samuellortie2001@gmail.com>
2025-12-10 14:26:29 -0500

    ajout de fonction afficher_entrees

--- menu.py
+++ menu.py
@@ -3,3 +3,11 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+def afficher_entrees():
+    entrees = ["Salade ", "Ailes de poulet", "Tartare"]
+    print("====ENTRÉE====")
+    for i, entree in enumerate(entrees, 1):
+        print(f"{i}.{entree}")
+
+afficher_entrees()
